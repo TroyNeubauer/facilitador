16b9037ab872d180892cdd690121877c0a2d1747
Troy Neubauer <troyneubauer@gmail.com>
2022-02-07 14:15:09 -0500

    Update TX program

--- Software/camera/src/main.rs
+++ Software/camera/src/main.rs
@@ -3,7 +3,11 @@
 #![feature(bench_black_box)]
 
 use cortex_m_rt::entry;
-use hal::{prelude::*, gpio::Edge};
+use hal::{
+    gpio::Edge,
+    pac::{dcmi, DCMI},
+    prelude::*,
+};
 use panic_halt as _;
 use stm32f4xx_hal as hal;
 
@@ -15,6 +19,28 @@ fn main() -> ! {
     // Enable the clock for the DCMI and GPIOs
     device.RCC.ahb2enr.write(|w| w.dcmien().set_bit());
     device.RCC.ahb1enr.write(|w| w.gpioien().set_bit());
+
+    //(#) DCMI pins configuration
+    //  (++) Connect the involved DCMI pins to AF13 using the following function
+    //      GPIO_PinAFConfig(GPIOx, GPIO_PinSourcex, GPIO_AF_DCMI);
+    //  (++) Configure these DCMI pins in alternate function mode by calling
+    //      the function GPIO_Init();
+    //
+    // ???
+
+    device.DCMI.cr.write(|w| w);
+    // DCMI_InitStruct->DCMI_CaptureMode = DCMI_CaptureMode_Continuous;
+    // DCMI_InitStruct->DCMI_SynchroMode = DCMI_SynchroMode_Hardware;
+    // DCMI_InitStruct->DCMI_PCKPolarity = DCMI_PCKPolarity_Falling;
+    // DCMI_InitStruct->DCMI_VSPolarity = DCMI_VSPolarity_Low;
+    // DCMI_InitStruct->DCMI_HSPolarity = DCMI_HSPolarity_Low;
+    // DCMI_InitStruct->DCMI_CaptureRate = DCMI_CaptureRate_All_Frame;
+    // DCMI_InitStruct->DCMI_ExtendedDataMode = DCMI_ExtendedDataMode_8b;
+
+    // # Safety: We got the bits from a read of the register
+    let dcmi = device.DCMI;
+    //Looks like no high level wrapper exists for DCMI from HAL.
+    //We have to use the auto generated building blocks
 
     let rcc = device.RCC.constrain();
     let clocks = rcc
@@ -32,13 +58,13 @@ fn main() -> ! {
     // Initialize the different pins
     let gpioc = device.GPIOC.split();
     let gpiod = device.GPIOD.split();
-    let gpiob = device.GPIOB.split(); 
+    let gpiob = device.GPIOB.split();
 
     let mut delay = hal::delay::Delay::new(core.SYST, &clocks);
 
     //Configure camera with I2C
-    
+
     let dcmi = device.DCMI;
-    
+
     loop {}
 }
--- Software/radio_test_tx/src/main.rs
+++ Software/radio_test_tx/src/main.rs
@@ -5,52 +5,48 @@
 use cortex_m_rt::entry;
 use hal::{prelude::*, gpio::Edge};
 use panic_halt as _;
-use stm32f4xx_hal as hal;
+use stm32f1xx_hal as hal;
 
 #[entry]
 fn main() -> ! {
-    let mut device = hal::pac::Peripherals::take().unwrap();
+    let dp = hal::pac::Peripherals::take().unwrap();
     let core = cortex_m::Peripherals::take().unwrap();
+    let mut flash = dp.FLASH.constrain();
+    let mut afio = dp.AFIO.constrain();
 
-    let rcc = device.RCC.constrain();
+    let rcc = dp.RCC.constrain();
     let clocks = rcc
         .cfgr
-        .use_hse(26.mhz())
-        .require_pll48clk()
-        .sysclk(84.mhz())
-        .hclk(84.mhz())
-        .pclk1(21.mhz())
-        .pclk2(42.mhz())
-        .freeze();
+        .use_hse(8.mhz())
+        .sysclk(72.mhz())
+        .hclk(72.mhz())
+        .freeze(&mut flash.acr);
     
-    let mut syscfg = device.SYSCFG.constrain();
-
     // Initialize the different pins
-    let gpioc = device.GPIOC.split();
-    let gpiod = device.GPIOD.split();
-    let gpiob = device.GPIOB.split();
+    let mut gpioa = dp.GPIOA.split();
 
-    let cs = gpiod.pd2.into_push_pull_output();
-    let mut data_int = gpiob.pb3.into_pull_up_input();
-    data_int.make_interrupt_source(&mut syscfg);
-    data_int.enable_interrupt(&mut device.EXTI);
-    data_int.trigger_on_edge(&mut device.EXTI, Edge::Falling);
+    let cs = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
+    //let mut data_int = gpiob.pb3.into_pull_up_input();
+    //data_int.make_interrupt_source(&mut syscfg);
+    //data_int.enable_interrupt(&mut device.EXTI);
+    //data_int.trigger_on_edge(&mut device.EXTI, Edge::Falling);
 
-    let ce = gpiod.pd3.into_push_pull_output();
+    let ce = gpioa.pa3.into_push_pull_output(&mut gpioa.crl);
 
-    let mosi = gpioc.pc12.into_alternate();
-    let miso = gpioc.pc11.into_alternate();
-    let sclk = gpioc.pc10.into_alternate();
+    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+    let miso = gpioa.pa6;
+    let sclk = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
 
-    let spi = hal::spi::Spi::new(
-        device.SPI3,
+    let spi = hal::spi::Spi::spi1(
+        dp.SPI1,
         (sclk, miso, mosi),
+        &mut afio.mapr,
         nrf24_rs::SPI_MODE,
         1.mhz(),
-        &clocks,
+        clocks,
     );
 
-    let mut delay = hal::delay::Delay::new(core.SYST, &clocks);
+    let mut delay = hal::delay::Delay::new(core.SYST, clocks);
 
     let message = b"Hello world!"; // The message we will be sending
 
